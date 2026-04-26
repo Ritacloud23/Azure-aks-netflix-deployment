9e66aefbd9ac1c0391fd2ff13613a84a30b371ba
https://github.com/Ritacloud23 <rnnenna53@gmail.com>
2026-04-25 20:40:12 -0400

    added resolve AKS ingress connectivity and TLS certificate issuance

--- netflix_frontend/src/api/axiosConfig.js
+++ netflix_frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://172.214.28.235:8080',
+    baseURL: 'https://api.devopsbliss.online',
     headers: {
         'Content-Type': 'application/json',
     },
